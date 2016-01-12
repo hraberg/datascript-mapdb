96d5a6f01308994aae1fd1794781304d470b213a
Håkan Råberg <hakan.raberg@gmail.com>
2016-01-12 17:54:56 +0000

    Refactoring, fixing insert.

--- src/datascript_mapdb/art.clj
+++ src/datascript_mapdb/art.clj
@@ -8,96 +8,98 @@
   (lookup [this key-byte])
   (insert [this key-byte value]))
 
-(defrecord Leaf [key value])
+(defn key-position ^long [^long size ^"[B" keys ^long key-byte]
+  (Arrays/binarySearch keys 0 size (byte key-byte)))
 
-(defn key-position ^long [^long count ^"[B" keys ^long key-byte]
-  (Arrays/binarySearch keys 0 count (byte key-byte)))
-
-(defn lookup-helper [^long count keys ^objects nodes key-byte]
-  (let [pos (key-position count keys key-byte)]
+(defn lookup-helper [^long size keys ^objects nodes ^long key-byte]
+  (let [pos (key-position size keys key-byte)]
     (when-not (neg? pos)
       (aget nodes pos))))
 
-(defn grow-helper [^long count ^"[B" keys ^objects nodes node]
+(defn grow-helper [^long size ^"[B" keys ^objects nodes node]
   (loop [idx 0
          node node]
-    (if (< idx count)
+    (if (< idx size)
       (recur (inc idx) (insert node (aget keys idx) (aget nodes idx)))
       node)))
 
-(defn insert-helper [count ^"[B" keys ^objects nodes key-byte value make-node grow-node]
-  (let [pos (key-position count keys key-byte)
+(defn insert-helper [size ^"[B" keys ^objects nodes key-byte value make-node grow-node]
+  (let [pos (key-position size keys key-byte)
         new-keys (aclone keys)
         new-nodes (aclone nodes)
-        node-size (clojure.core/count keys)]
+        capacity (count keys)]
     (if (neg? pos)
-      (if (< (long count) node-size)
+      (if (< (long size) capacity)
         (let [pos (-> pos inc Math/abs)]
           (aset new-keys pos (byte key-byte))
           (aset new-nodes pos value)
           (loop [idx (inc pos)]
-            (when (< idx node-size)
+            (when (< idx capacity)
               (aset new-keys idx (aget keys (dec idx)))
               (aset new-nodes idx (aget nodes (dec idx)))
               (recur (inc idx))))
-          (make-node (inc (long count)) new-keys new-nodes))
-        (insert (grow-helper count keys nodes (grow-node)) key-byte value))
-      (make-node count
+          (make-node (inc (long size)) new-keys new-nodes))
+        (insert (grow-helper size keys nodes (grow-node)) key-byte value))
+      (make-node size
                  (doto new-keys (aset pos (byte key-byte)))
                  (doto new-nodes (aset pos value))))))
 
 (declare make-node16 make-node48 make-node256)
 
-(defrecord Node4 [^long count ^"[B" keys ^objects nodes]
+(defrecord Node4 [^long size ^"[B" keys ^objects nodes]
   ARTNode
   (lookup [this key-byte]
-    (lookup-helper count keys nodes key-byte))
+    (lookup-helper size keys nodes key-byte))
 
   (insert [this key-byte value]
-    (insert-helper count keys nodes key-byte value ->Node4 make-node16)))
+    (insert-helper size keys nodes key-byte value ->Node4 make-node16)))
 
-(defrecord Node16 [^long count ^"[B" keys ^objects nodes]
+(defrecord Node16 [^long size ^"[B" keys ^objects nodes]
   ARTNode
   (lookup [this key-byte]
-    (lookup-helper count keys nodes key-byte))
+    (lookup-helper size keys nodes key-byte))
 
   (insert [this key-byte value]
-    (insert-helper count keys nodes key-byte value ->Node16 make-node48)))
+    (insert-helper size keys nodes key-byte value ->Node16 make-node48)))
 
-(defrecord Node48 [^long count ^"[B" key-index ^objects nodes]
+(defrecord Node48 [^long size ^"[B" key-index ^objects nodes]
   ARTNode
   (lookup [this key-byte]
     (let [pos (aget key-index (byte key-byte))]
-      (when (< pos (clojure.core/count nodes))
+      (when (< pos (count nodes))
         (aget nodes pos))))
 
   (insert [this key-byte value]
     (let [idx (aget key-index key-byte)
-          idx (byte (if (= (clojure.core/count nodes) idx)
-                      (inc count)
-                      idx))
-          node-size (clojure.core/count nodes)]
-      (if (< idx node-size)
-        (make-node48 idx
+          capacity (count nodes)
+          new-key? (= capacity idx)
+          idx (byte (if new-key?
+                      size
+                      idx))]
+      (if (< idx capacity)
+        (make-node48 (cond-> idx
+                       new-key? inc)
                      (doto (aclone key-index) (aset (byte key-byte) idx))
                      (doto (aclone nodes) (aset idx value)))
         (loop [key 0
                node (make-node256)]
-          (if (< key (clojure.core/count key-index))
+          (if (< key (count key-index))
             (let [idx (aget key-index key)]
               (recur (inc key) (cond-> node
-                                 (< idx node-size) (insert key (aget nodes idx)))))
+                                 (< idx capacity) (insert key (aget nodes idx)))))
             (insert node key-byte value)))))))
 
-(defrecord Node256 [^long count ^objects nodes]
+(defrecord Node256 [^long size ^objects nodes]
   ARTNode
   (lookup [this key-byte]
     (aget nodes key-byte))
 
   (insert [this key-byte value]
-    (->Node256 (cond-> count
+    (->Node256 (cond-> size
                  (nil? (aget nodes key-byte)) inc)
                (doto (aclone nodes) (aset key-byte value)))))
+
+(defrecord Leaf [^"[B" key value])
 
 (defn make-node4 []
   (->Node4 0 (byte-array 4 (byte 4)) (object-array 4)))
@@ -123,14 +125,27 @@
       (let [^Leaf leaf node]
         (when (Arrays/equals key-bytes (bytes (.key leaf)))
           (.value leaf)))
-      (when node
+      (when (and node (< idx (count key-bytes)))
         (recur (inc idx) (lookup node (aget key-bytes idx)))))))
 
+;; Strings needs to be 0 terminated, see IV. CONSTRUCTING BINARY-COMPARABLE KEYS
 (defn art-insert [tree ^"[B" key-bytes value]
   (loop [idx 0
          node tree]
-    (let [child (lookup node (aget key-bytes idx))]
+    (let [child (some-> node (lookup (aget key-bytes idx)))]
       (if (and (satisfies? ARTNode child)
-               (< (clojure.core/count key-bytes) (dec idx)))
+               (< (count key-bytes) (dec idx)))
         (recur (inc idx) child)
-        (insert node (aget key-bytes idx) (->Leaf key-bytes value))))))
+        (insert node (aget key-bytes idx)
+                (if (or (nil? child)
+                        (Arrays/equals key-bytes (bytes (.key ^Leaf child))))
+                  (->Leaf key-bytes value)
+                  (-> (art-make-tree)
+                      (insert (aget key-bytes (inc idx)) (->Leaf key-bytes value))
+                      (insert (aget (bytes (.key ^Leaf child)) (inc idx)) child))))))))
+
+(comment
+  (-> (art-make-tree)
+      (art-insert (byte-array [2 3 0]) "boo")
+      (art-insert (byte-array [2 0]) "baz")
+      (art-lookup (byte-array [2 0]))))
