2c9b0e19408f43160a4545d818ab21b7de48d064
Håkan Råberg <hakan.raberg@gmail.com>
2016-01-12 18:28:14 +0000

    Bug.

--- src/datascript_mapdb/art.clj
+++ src/datascript_mapdb/art.clj
@@ -137,7 +137,7 @@
          node (or tree (art-make-tree))]
     (let [child (some-> node (lookup (aget key-bytes idx)))]
       (if (and (satisfies? ARTNode child)
-               (< (count key-bytes) (dec idx)))
+               (< idx (count key-bytes)))
         (recur (inc idx) child)
         (insert node (aget key-bytes idx)
                 (if (or (nil? child) (leaf-matches-key? child key-bytes))
