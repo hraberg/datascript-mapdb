d3317e4bad09bdafa7325ef96a19e9af084fd9be
Håkan Råberg <hakan.raberg@gmail.com>
2016-01-19 19:57:34 +0000

    Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/hraberg/datascript-mapdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [datascript "0.13.3"]
                  [org.mapdb/mapdb "2.0-beta12"]
                  [com.taoensso/nippy "2.11.0-beta1"]]
